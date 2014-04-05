4edd506e0b4bfc95f2b95575b2af40782bde3ffa
Adam Hartwell <adamhartwell2@gmail.com>
2014-04-05 19:08:54 +0100

    Initial Commit
    Function shell + README

---
+++ README.md
@@ -0,0 +1,11 @@
+read-time
+=========
+
+A simple function that takes in a HTML marked up or plain text string and estimates the time it will take to read. Returns in seconds
+
+Use
+========
+
+Take the function and add it to your code then use as you wish. Has no dependencies.
+
+
---
+++ read-time.js
@@ -0,0 +1,7 @@
+/*** Take in a string (plain text or HTML marked) and estimate read time ***/
+function estReadTime(string){
+	var readSpeed = 250;      // Estimated speed of reader in Words Per Minute (wpm)
+	var imageTime = 10;        // Estimate of time taken up by pictures (seconds)
+
+	var readSpeed = 250/60; // Convert to words per second
+}
