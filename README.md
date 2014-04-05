916c853dfadfa2350fb444f4a2dd052442cc9333
Adam Hartwell <adamhartwell2@gmail.com>
2014-04-05 21:06:10 +0100

    Reading time for images implemented
    Also updated README

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 read-time
 =========
 
-A simple function that takes in a HTML marked up or plain text string and estimates the time it will take to read. Returns in seconds
+A simple function that takes in a HTML marked up or plain text string (I've been using it for blog articles from WordPress) and estimates the time it will take to read. Returns in seconds
 
 Use
 ========
--- read-time.js
+++ read-time.js
@@ -1,7 +1,15 @@
-/*** Take in a string (plain text or HTML marked) and estimate read time ***/
-function estReadTime(string){
-	var readSpeed = 250;      // Estimated speed of reader in Words Per Minute (wpm)
-	var imageTime = 10;        // Estimate of time taken up by pictures (seconds)
+/*** Take in an article as a string (plain text or HTML marked) and estimate read time ***/
+function estReadTime(article){
+	var readSpeed = 250;           // Estimated speed of reader in Words Per Minute (wpm)
+	var imageTime = 10;             // Estimate of time taken up by pictures (seconds)
 
-	var readSpeed = 250/60; // Convert to words per second
+	var readTime = 0;                 // Always have something to return
+
+	readSpeed = readSpeed/60; // Convert to words per second
+
+	var count = article.match(/<img/gi);
+
+	readTime += count.length * imageTime;
+
+	return readTime;
 }
