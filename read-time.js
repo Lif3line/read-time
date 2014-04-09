6ca95e4cc42709d275ebf547bae8b2eefb1eaa8f
Adam Hartwell <adamhartwell2@gmail.com>
2014-04-09 12:25:26 +0100

    Added read time based on number of words

--- read-time.js
+++ read-time.js
@@ -1,11 +1,10 @@
 /*** Take in an article as a string (plain text or HTML marked) and estimate read time ***/
 function estReadTime(article){
 	var readSpeed = 250;           // Estimated speed of reader in Words Per Minute (wpm)
+	readSpeed = readSpeed/60; // Convert to words per second
 	var imageTime = 10;             // Estimate of time taken up by pictures (seconds)
 
 	var readTime = 0;                 // Always have something to return
-
-	readSpeed = readSpeed/60; // Convert to words per second
 
 	var count = article.match(/<img/gi);
 	if(count) {                            // Catch case where count returns as null on no matches
@@ -15,6 +14,7 @@ function estReadTime(article){
 	article = article.replace(/<.*?>/gi, "");    // Remove mark up
 	article = article.replace(/&nbsp;/gi, " "); // Replace non-breaking whitespace markup with real whitespace
 
+	readTime += article.split(/\s+/).length / readSpeed;
 
 	return readTime;
 }
